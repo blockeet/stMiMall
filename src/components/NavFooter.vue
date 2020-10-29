e0dc0f010676486453c67d8ba156b5493ae4ec0d
blockeet <394405532@qq.com>
2020-10-29 09:49:58 +0800

    页面footer组件的构建

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/pngs/logo-footer.png" alt="小米商城">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="" target="_blank">链接</a><span>|</span>
+      <a href="" target="_blank">链接</a><span>|</span>
+      <a href="" target="_blank">链接</a><span>|</span>
+      <a href="" target="_blank">链接</a>
+    </div>
+    <div class="copyright">Copyright  ©2020 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer'
+}
+</script>
+
+<style lang="less">
+  .footer{
+    height: 234px;
+    border-top: 4px solid #ff6600;
+    background-color: #333333;
+    color: #999999;
+    font-size: 14px;
+    text-align: center;
+    .footer-logo{
+      margin-top: 46px;
+      margin-bottom: 31px;
+      img{
+        width: 53px;
+        height: 36px;
+        margin-bottom: 13px;
+      }
+    }
+    .footer-link{
+      a{
+        color: #999999;
+        display: inline-block;
+      }
+      span{
+        margin: 0 10px;
+      }
+      margin-bottom: 13px;
+    }
+    .copyright{
+      .domain{
+        color: #ff6600
+      }
+    }
+  }
+</style>
